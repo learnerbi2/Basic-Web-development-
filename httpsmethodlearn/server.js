7039304e69733303438c6b73dc459a68d52fbc9d
learnerbi2 <139643732+learnerbi2@users.noreply.github.com>
2025-09-25 19:10:46 +0530

    Add files via upload

---
+++ httpsmethodlearn/server.js
@@ -0,0 +1,21 @@
+//type :module
+//check imports
+import {w,b} from './module.js';
+import {sum} from './module.js';
+import obj from './module.js';
+import Anything from './module.js' //this will import default value
+
+console.log(b)
+console.log(w)
+console.log(sum(2,3))
+console.log(Anything)
+console.log(c)
+
+
+
+// var slugify = require('slugify') 
+// let A = 20
+// let b = 10
+// console.log(A+b)
+// let t = slugify('some string','_')
+// console.log(t)
